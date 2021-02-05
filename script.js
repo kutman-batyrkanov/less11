1e6a6070004233f4d1c5b7ae0f4fdacf5b50be81
Your Name <you@example.com>
2021-02-05 15:07:22 +0600

    let style1

---
+++ script.js
@@ -0,0 +1,46 @@
+// let h1 = document.createElement('h1');
+// h1.innerText = "JavaScript styling";
+// document.body.append(h1);
+
+// let ul = document.createElement('ul');
+// for (let i = 1; i <= 5; i++) {
+//     let li = document.createElement('li');
+//     let button = document.createElement('button');
+//     button.innerText = "Example";
+//     button.id = " example" + i;
+
+//     li.append(button);
+//     ul.append(li);
+// }
+
+// document.body.append(ul);
+
+
+let style1 = document.querySelector('#style1');
+style1.style.backgroundColor = "white";
+style1.style.width = "150px";
+style1.style.height = "50px";
+style1.style.margin = "10px";
+
+let style2 = document.querySelector("#style2");
+style2.style.backgroundColor = "pink";
+style2.style.border = "white";
+style2.style.width = "100px";
+style2.style.height = "50px";
+style2.style.margin = "10px";
+
+let style3 = document.querySelector("#style3");
+style3.style.backgroundColor = "white";
+style3.style.border = "dotted 2px black";
+style3.style.width = "170px";
+style3.style.height = "40px";
+style3.style.margin = "10px";
+
+
+
+let style4 = document.querySelector("#style4");
+style4.style.backgroundColor = "white";
+style4.style.border = "dotted 2px red";
+style4.style.width = "80px";
+style4.style.height = "20px";
+style4.style.margin = "10px";
